82663bf70d287ade101c55bbe9c8a3dc594cb974
Jintaimeng <929357244@qq.com>
2020-05-17 16:26:16 +0800

    bfs shortestPath

--- ShortestPath.h
+++ ShortestPath.h
@@ -1,37 +1,49 @@
 template<typename Graph>
 
-class Path{
+class ShortestPath{
 private:
 	Graph &G;
-	bool *visited;
 	int s;
+	bool *visited;
 	int *from;
-	void dfs(int v){
-		visited[v] = true;
-		typename Graph::adjIterator adj(G, v);
-		for(int i = adj.begin() ; !adj.end() ; i = adj.next()){
-			if( !visited[i] ){
-				from[i] = v;
-				dfs(i);
-			}
-		}
-	}
+	int *ord;
 	
 public:
-	Path(Graph &graph, int s): G(graph){
-		assert( s>=0&&s<G.V() ); 
-		visited = new bool[G.V()];	
-		from = new int[G.V()];		
+	ShortestPath(Graph &graph, int s): G(graph){
+		assert( s>=0&&s<G.V() );
+		visited = new bool[G.V()];
+		from = new int[G.V()];
+		ord = new int[G.V()];
 		for(int i = 0 ; i < G.V() ; i ++){
 			visited[i] = false;
 			from[i] = -1;
+			ord[i] = -1;
 		}
 		this->s = s;
-		dfs(s);
+		
+		queue<int> q;
+		//无向图最短路径算法
+		q.push(s);
+		visited[s] = true;
+		ord[s] = 0; 
+		while( !q.empty() ){
+			int v = q.front();
+			q.pop();
+			typename Graph::adjIterator adj(G, v);
+			for( int i = adj.begin() ; !adj.end() ; i = adj.next() ){
+				if( !visited[i] ){
+					q.push(i);
+					visited[i] = true;
+					from[i] = v;
+					ord[i] = ord[v]+1;
+				}
+			}
+		}
 	}
-	~Path(){
+	~ShortestPath(){
 		delete[] visited;
 		delete[] from;
+		delete[] ord;
 	}
 	bool hasPath(int w){
 		assert( w>=0&&w<G.V() ); 
@@ -48,8 +60,7 @@ public:
 		while( !s.empty() ){
 			vec.push_back( s.top() ) ;
 			s.pop;
-		}
-		
+		}		
 	}
 	void showPath(int w){
 		vector<int> vec;
@@ -62,5 +73,9 @@ public:
 			else
 				cout<<" --> ";
 		}
+	}
+	int length(int w){
+		assert( w>=0&&w<G.V() );
+		return ord[w];
 	}
 };
